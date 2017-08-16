6cde5bc6f4d35e90f36cbb9404cd809cbe30482d
vesselov <vesselov.zhenya@gmail.com>
2017-08-16 09:53:03 +0500

    burger

---
+++ (img/js/main.js
@@ -0,0 +1,10 @@
+window.onload
+function changePanel() {
+    var panel = document.getElementsByClassName("menu-list")
+    var panelTitle = document.getElementsByClassName("menu-list__item")
+    if (panel.style.display == "none") {
+        panel.style.display = ""
+
+    }
+
+}
