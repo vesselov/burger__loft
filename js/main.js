7344bf87a055a7dee1b5caf1c1ae91ede23862ad
vesselov <vesselov.zhenya@gmail.com>
2017-08-17 17:33:36 +0500

    burger

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-$(document).ready(function() {
-    $('.accordion__crew').hide(e);
-    $('.accordion__title').click,
-        function(e) {
-            e.preventDefault()
-            var $answer = $(this).next('.accordion__crew');
-            if ($answer.is(':hidden')) {
-                $answer.slideDown();
-                $(this).addClass('active');
-            } else {
-                $answer.fadeOut();
-                $(this).removeClass('active');
-            }
-        }; // окончание click
-}); // окончание ready
-
 $(function() {
     $('.menu-list').on('click', function(e) {
 
@@ -25,6 +9,30 @@ $(function() {
             items = item.siblings(),
             otherContent = items.find('.menu-list__item');
         e.preventDefault()
+
+        if (!item.hasClass('active')) {
+            items.removeClass('active');
+            item.addClass('active');
+
+
+        } else {
+            item.removeClass('active')
+
+        }
+    })
+});
+
+$(function() {
+    $('.accordion__title').on('click', function(e) {
+        e.preventDefault()
+
+        var elem = $(e.target),
+            item = elem.closest('.accordion__list'),
+            content = item.find('.accordion__crew'),
+            // reqHeight = item.find('.menu-list__text').outerHeight(),
+            items = item.siblings(),
+            otherContent = items.find('.accordion__list');
+
 
         if (!item.hasClass('active')) {
             items.removeClass('active');
